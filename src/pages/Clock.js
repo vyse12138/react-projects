f4b26a226f292787adec33d9bf494f1ff5066527
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-05 15:10:33 +1100

    Counter update

--- src/pages/Clock.js
+++ src/pages/Clock.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef} from 'react';
 
 function Clock() {
-
+    //retrieve date data and store them in using state hooks
     let date = new Date();
     const [year, setYear] = useState(date.getFullYear().toString().padStart(2, '0'));
     const [month, setMonth] = useState((date.getMonth() + 1).toString().padStart(2, '0'));
@@ -9,8 +9,9 @@ function Clock() {
     const [hour, setHour] = useState(date.getHours().toString().padStart(2, '0'));
     const [minute, setMinute] = useState(date.getMinutes().toString().padStart(2, '0'));
     const [second, setSecond] = useState(date.getSeconds().toString().padStart(2, '0'));
-    useEffect(() => {
+    let a = useEffect(() => {
         let timer =  setInterval(() => {
+            //retrieve adn set date data each second
             let date = new Date();
             setYear(date.getFullYear().toString().padStart(2, '0'));
             setMonth((date.getMonth() + 1).toString().padStart(2, '0'));
@@ -19,36 +20,69 @@ function Clock() {
             setMinute(date.getMinutes().toString().padStart(2, '0'));
             setSecond(date.getSeconds().toString().padStart(2, '0'));
         }, 1000);
-
         return () => clearInterval(timer);
+    });
 
-    })
-
-    const [currentTime, setCurrentTime] = useState();
+    //representing current time
+    const [currentTime, setCurrentTime] = useState('');
     const handleOnGetTime = () =>{
         setCurrentTime(`${hour}:${minute}:${second}`);
     }
 
-    const [count, setCount] = useState(0);
-
-    const counter = useRef(null)
-    const handleOnStart = () =>{
+    //counter datas
+    const [count, setCount] = useState('');
+    const [countStarted, setCountStarted] = useState(false);
+    const [countMinute, setCountMinute] = useState(0);
+    const [countSecond, setCountSecond] = useState(0);
+    const [countMilisecond, setCountMilisecond] = useState(0);
+    //counter useEffect hook
+    useEffect(() => {
         
-        counter.current = setInterval(() => {
-            setCount((count) => count +1);
+        let counter;
+        //check if counter is enabled
+        if (countStarted){
+            //tick the counter each second
+            counter = setInterval(() => {
+                setCountSecond(countSecond => countSecond + 1);
+                if (countSecond  == 59){
+                    setCountSecond(0);
+                    setCountMinute(countMinute => countMinute + 1);
+                } 
+            }, 1000);
 
-        }, 1000);
+        }
+        return () => clearInterval(counter);
+
+    },[countStarted,countSecond]);
+
+    //counter milisecond mimic, not accurate
+    useEffect(() => {
+        
+        let counter;   
+        if (countStarted){
+            counter = setInterval(() => {
+                setCountMilisecond(Math.floor(Math.random()*100));
+
+            }, 5);
+        }
+        return () => clearInterval(counter);
+    },[countStarted,countMilisecond]);
+
+    const handleOnClear = () =>{
+        setCountStarted(false);
+        setCountMinute(0);
+        setCountSecond(0);
+        setCountMilisecond(0);
     }
 
-    const handleOnStop = () =>{
-        clearInterval(counter.current)
-    }
 
 
-    const handleOnReset = () =>{
-        clearInterval(counter.current);
-        setCount(0);
-    }
+    let counts = (
+        `${countMinute.toString().padStart(2, '0')}:
+        ${countSecond.toString().padStart(2, '0')}.
+        ${countMilisecond.toString().padStart(2, '0')}`)
+    
+    //clock elements
     let clockDate =`${year}/${month}/${day}`;
     let clockTime = (date.getSeconds()%2) 
                     ? `${hour}:${minute}:${second}`
@@ -59,18 +93,15 @@ function Clock() {
             <div className="time text-center text-dark  display-1 font-weight-bold pb-2">{clockTime}</div>
 	    
             <div className="container row  no-gutters bg-light border shadow py-2 my-2">
+                <h4 className='col-2 my-1 mx-3'>{currentTime}</h4>
                 <button className='btn btn-primary' onClick={()=>handleOnGetTime()}>Get Time</button>
-
-                <h4 className='  mx-3'>{currentTime}</h4>
 	        </div>
-            <div className="container row  no-gutters bg-light border shadow py-2 my-2">
-                
-                
-                <h4 className='col-1 my-1 mx-3'>{count}</h4>
 
-                <button className='btn btn-success mr-3' onClick={()=>handleOnStart()}>Start</button>
-                <button className='btn btn-warning mr-3' onClick={()=>handleOnStop()}>Stop</button>
-                <button className='btn btn-danger' onClick={()=>handleOnReset()}>Reset</button>
+            <div className="container row  no-gutters bg-light border shadow py-2 my-2">
+                <h4 className='col-2 my-1 mx-3'>{counts}</h4>
+                <button className='btn btn-success mr-3' onClick={()=>setCountStarted(true)}>Start</button>
+                <button className='btn btn-warning mr-3' onClick={()=>setCountStarted(false)}>Stop</button>
+                <button className='btn btn-danger' onClick={()=>handleOnClear()}>Reset</button>
 
 	        </div>
         </div>
