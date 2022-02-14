09c5315bf52e138d5741c321f11b4541740120a1
Yulei Zhu <zhuyulei12333@gmail.com>
2022-02-14 22:27:00 +1100

    build: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-  assetsInlineLimit: 0
-})
+  builld: {
+    assetsInlineLimit: 0
+  }
+}
